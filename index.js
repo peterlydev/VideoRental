0bbdeddc340231e8442158d6cd9c9dcb06b4bc3b
Peter Ly <peter.ly1@hotmail.com>
2023-01-18 17:58:20 +0000

    updated index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 const express = require('express');
 const app = express();
 
@@ -16,11 +17,12 @@ mongoose
   .catch(() => console.log("Could not connect to MongoDB!"));
 
 app.use(express.json());
-app.use("/", home);
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals)
+app.use('/', home);
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals),
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
