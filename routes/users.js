8f6b5a9355e12402d7fde5df80542d14b1594868
Peter Ly <peter.ly1@hotmail.com>
2023-01-18 17:56:11 +0000

    Created post route on users

--- routes/users.js
+++ routes/users.js
@@ -3,3 +3,9 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
   
+router.post('/', async (req, res) => {
+    const users = await User.find().sort('name');
+    res.send(users);
+});
+
+module.exports = router;
