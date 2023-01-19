a3e084078a729b06624d9ab11acb07949f0e161a
Peter Ly <peter.ly1@hotmail.com>
2023-01-19 22:58:59 +0000

    created new schema and added a JWT function to the User file

--- index.js
+++ index.js
@@ -13,12 +13,10 @@ const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
-
 if(!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined!');
   process.exit(1);
 } 
-
 
 mongoose
   .connect("mongodb://localhost/vidly")
