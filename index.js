16bde634da9fe36ccb5c187d2ecf58bd2a669988
Peter Ly <peter.ly1@hotmail.com>
2023-01-19 19:13:18 +0000

    made minor changes to auth

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+const config = require('config');
 const Joi = require("joi");
-const jwt = require('jsonwebtoken');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require("mongoose");
 const Fawn = require('fawn');
--- routes/auth.js
+++ routes/auth.js
@@ -18,10 +18,8 @@ router.post('/', async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password)
   if (!validPassword) return res.status(400).send('Invalid email or password!')
 
-  const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+  const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey');
   res.send(token);
-  
-  res.send(true);
 });
 
   function validate(req) {
