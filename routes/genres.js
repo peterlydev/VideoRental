d10b5e085d32862ae216c333fb7affeb5ec5d75e
Peter Ly <peter.ly1@hotmail.com>
2023-01-20 17:26:37 +0000

    Adding auth to the genres.js

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,5 @@
+
+const auth = require('../middleware/auth');
 const { Genre, validate } = require('../models/genre');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -17,7 +19,7 @@ const router = express.Router();
   });
   
     
-  router.post("/", async (req, res) => {    
+  router.post("/", auth, async (req, res) => {    
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
   
