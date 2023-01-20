0bc56a4c05b05bf452bd39506216172e2f1bcb2e
Peter Ly <peter.ly1@hotmail.com>
2023-01-20 23:26:28 +0000

    Added middleware on users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
+const auth = require('../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
+
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
