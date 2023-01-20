f1d6b4ae70229373b158c2c827abbd5e1676a2b8
Peter Ly <peter.ly1@hotmail.com>
2023-01-20 17:23:22 +0000

    Created Middleware function so authenticated users can call API endpoints

--- routes/genres.js
+++ routes/genres.js
@@ -17,7 +17,7 @@ const router = express.Router();
   });
   
     
-  router.post("/", async (req, res) => {
+  router.post("/", async (req, res) => {    
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
   
