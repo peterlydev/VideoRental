7e1d668db89176c30d4e88742fe3abaa77529a18
Peter Ly <peter.ly1@hotmail.com>
2023-01-23 21:35:53 +0000

    installed winston-mongodb to transport logs to mongo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('express-async-errors');
 const winston = require('winston');
+require('winston-mongodb');
 const error = require('./middleware/error');
 const config = require('config');
 const Joi = require("joi");
--- routes/genres.js
+++ routes/genres.js
@@ -6,7 +6,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  throw new Error("Could not get genres.");
   const genres = await Genre.find().sort("name");
   res.send(genres);
 });
