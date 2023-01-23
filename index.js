0908d46551fa6d4c09a91b355f045ab07a30dd73
Peter Ly <peter.ly1@hotmail.com>
2023-01-23 22:25:01 +0000

    added uncaught exceptions

--- index.js
+++ index.js
@@ -17,11 +17,18 @@ const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
 
+process.on('uncaughtException', (ex) => {
+  console.log("WE GOT AN UNCAUGHT EXCEPTION");
+  winston.error(ex.message, ex);
+})
+
 winston.add(winston.transports.File, { filename: "logfile.log" });
 winston.add(winston.transports.MongoDB, {
   db: "mongodb://localhost/vidly",
   level: "info"
 });
+
+throw new Error('Something failed during startup.');
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined!");
