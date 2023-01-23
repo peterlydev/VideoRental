d0ee569876a07b0c607444049ef0311ea6169536
Peter Ly <peter.ly1@hotmail.com>
2023-01-23 23:18:26 +0000

    Refactoring complete

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const config = require("config");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
 
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
-require('./startup/logging')();
-
-
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined!");
-  process.exit(1);
-}
+require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
