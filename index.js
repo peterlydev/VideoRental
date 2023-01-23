bff8be8e3f55940eb4f3a9f80534cedd921ad05b
Peter Ly <peter.ly1@hotmail.com>
2023-01-23 22:19:08 +0000

    added a level to the transport and set it to info

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb');
-const error = require('./middleware/error');
-const config = require('config');
+require("express-async-errors");
+const winston = require("winston");
+require("winston-mongodb");
+const error = require("./middleware/error");
+const config = require("config");
 const Joi = require("joi");
-Joi.objectId = require('joi-objectid')(Joi);
+Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
-const Fawn = require('fawn');
-const home = require('./routes/home')
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const express = require('express');
+const Fawn = require("fawn");
+const home = require("./routes/home");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const express = require("express");
 const app = express();
 
-winston.add(winston.transports.File, { filename: 'logfile.log' });
-winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' });
+winston.add(winston.transports.File, { filename: "logfile.log" });
+winston.add(winston.transports.MongoDB, {
+  db: "mongodb://localhost/vidly",
+  level: "info"
+});
 
-if(!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined!');
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined!");
   process.exit(1);
-} 
+}
 
 mongoose
   .connect("mongodb://localhost/vidly")
@@ -31,13 +34,13 @@ mongoose
   .catch(() => console.log("Could not connect to MongoDB!"));
 
 app.use(express.json());
-app.use('/', home);
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
+app.use("/", home);
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 app.use(error);
 
 const port = process.env.PORT || 3000;
