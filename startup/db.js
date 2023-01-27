51960c46d2df9c499cb8e2670d8c898f94989f72
Peter Ly <peter.ly1@hotmail.com>
2023-01-27 23:55:30 +0000

    preparing for integration tests

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,10 @@
 const winston = require('winston');
 const mongoose = require("mongoose");
+const config = require('config');
 
 module.exports = function () {
-    mongoose.connect()
-    .then(() => winston.info("Connected to MongoDB..."));
+    const db = config.get('db');
+    mongoose.connect(db)
+    .then(() => winston.info(`Connected to ${db}...`));
 
 };
