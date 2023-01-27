620c96000a8e1cd0aac6353939cd2540ae60c22f
Peter Ly <peter.ly1@hotmail.com>
2023-01-27 23:47:27 +0000

    preparing for integration tests

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const mongoose = require("mongoose");
 
 module.exports = function () {
-    mongoose.connect("mongodb://localhost/vidly")
+    mongoose.connect()
     .then(() => winston.info("Connected to MongoDB..."));
 
 };
