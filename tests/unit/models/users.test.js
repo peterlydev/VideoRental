2219257f56fd73c00a5bf57e220d84048e0fda29
Peter Ly <peter.ly1@hotmail.com>
2023-01-27 23:27:08 +0000

    test passed

--- tests/unit/models/users.test.js
+++ tests/unit/models/users.test.js
@@ -2,12 +2,17 @@ const { items } = require('joi/lib/types/array');
 const jwt = require('jsonwebtoken');
 const { User } = require('../../../models/user');
 const config = require('config');
+const mongoose = require('mongoose');
 
 describe('user.generateAuthToken', () => {
     it('should return a valid JWT', () => {
-        const user = new User({ _id: 1, isAdmin: true });
+        const payload = { 
+            _id: new mongoose.Types.ObjectId().toHexString(),
+            isAdmin: true 
+        };
+        const user = new User(payload);
         const token = user.generateAuthToken();
         const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-        expect(decoded).toMatchObject({ _id: 1, isAdmin: true });
+        expect(decoded).toMatchObject(payload);
     });
 });
