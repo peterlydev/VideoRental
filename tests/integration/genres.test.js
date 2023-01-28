4d09993a198c3497f74cda71493254acbfd1c872
Peter Ly <peter.ly1@hotmail.com>
2023-01-28 15:41:31 +0000

    Added new test

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -66,16 +66,43 @@ describe("/api/genres", () => {
     });
 
     it("should return a 400 if genre is more than 50 characters", async () => {
-        const token = new User().generateAuthToken();
+      const token = new User().generateAuthToken();
 
-        const name = new Array(52).join('a');
+      const name = new Array(52).join("a");
 
-        const res = await request(server)
-          .post("/api/genres")
-          .set('x-auth-token', token)
-          .send({ name: name});
-  
-        expect(res.status).toBe(400);
-      });
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: name });
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should save the genre if it is valid", async () => {
+      const token = new User().generateAuthToken();
+
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: "genre1" });
+
+      const genre = await Genre.find({ name: "genre1" });
+
+      expect(genre).not.toBeNull();
+    });
+
+    it("should return the genre if it is valid", async () => {
+      const token = new User().generateAuthToken();
+
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: "genre1" });
+
+      const genre = await Genre.find({ name: "genre1" });
+
+      expect(res.body).toHaveProperty('_id');
+      expect(res.body).toHaveProperty('name');
+    });
   });
 });
