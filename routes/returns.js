86069d95056860fee43d09176a1e69252df44dfa
Peter Ly <peter.ly1@hotmail.com>
2023-01-28 18:23:37 +0000

    Adding test cases

--- routes/returns.js
+++ routes/returns.js
@@ -1,11 +1,19 @@
+const { Rental } = require("../models/rental");
 const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-    if (!req.body.customerId) return res.status(400).send('CustomerID not valid!');
-    if (!req.body.movieId) return res.status(400).send('MovieID not valid!');
+  if (!req.body.customerId)
+    return res.status(400).send("CustomerID not valid!");
+  if (!req.body.movieId) return res.status(400).send("MovieID not valid!");
 
-    res.status(401).send('Unauthorized');
+  const rental = await Rental.findOne({
+    "customer._id": req.body.customerId,
+    "movie._id": req.body.movieId,
+  });
+  if (!rental) return res.status(404).send("Rental not found!");
+
+  res.status(401).send("Unauthorized");
 });
 
 module.exports = router;
