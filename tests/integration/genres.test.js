bcf45efdb0dc04a02d285e487291c39c611643e6
Peter Ly <peter.ly1@hotmail.com>
2023-01-28 14:40:11 +0000

    adding more tests

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,14 +1,28 @@
-const request = require('supertest');
-let server = require('../../index'); 
+const request = require("supertest");
+const { Genre } = require("../../models/genre");
+let server;
 
-describe('/api/genres', () => {
-    beforeEach(() => { server = require('../../index'); })
-    afterEach(() => { server.close(); });
+describe("/api/genres", () => {
+  beforeEach(() => {
+    server = require("../../index");
+  });
+  afterEach(async () => {
+    server.close();
+    await Genre.remove({});
+  });
 
-    describe('GET /', () => {
-        it('should return all genres', async () => {
-            const res = await request(server).get('/api/genres');
-            expect(res.status).toBe(200);
-        });
+  describe("GET /", () => {
+    it("should return all genres", async () => {
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+      ]);
+
+      const res = await request(server).get("/api/genres");
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+      expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
     });
+  });
 });
